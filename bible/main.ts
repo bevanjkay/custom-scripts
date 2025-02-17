1e43e8e40cdac4734747362a34fce46f2e151579
Bevan Kay <email@bevankay.me>
2025-02-17 12:04:29 +1100

    main.ts: fix input, lint

--- bible/main.ts
+++ bible/main.ts
@@ -1,28 +1,27 @@
 import { fetchReferenceContent } from "youversion-suggest";
 
-const input = Deno.args.slice(2).join(" ");
+const input = Deno.args.join(" ");
 
 if (!input) {
-    Deno.exit();
+  Deno.exit();
 }
 
 try {
+  const reference = await fetchReferenceContent(input, {
+    language: "eng", // Optional (default: 'eng')
+    fallbackVersion: "nlt", // Optional (default: 'niv')
+    // includeVerseNumbers: true // Optional (default: false)
+    // includeLineBreaks: false // Optional (default: true)
+  });
 
-    const reference = await fetchReferenceContent(input, {
-        language: "eng", // Optional (default: 'eng')
-        fallbackVersion: "nlt" // Optional (default: 'niv')
-        // includeVerseNumbers: true // Optional (default: false)
-        // includeLineBreaks: false // Optional (default: true)
-    });
-
-    const output = `${reference.name} (${reference.version.name})\n${reference.content}`
-    console.log(output);
+  const output =
+    `${reference.name} (${reference.version.name})\n${reference.content}`;
+  console.log(output);
 } catch (error: unknown) {
-    if (error instanceof Error) {
-        console.error("Query:", input, " | Error:", error.message);
-    } else {
-        console.error("Query:", input, " | Error:", error);
-    }
-    Deno.exit();
+  if (error instanceof Error) {
+    console.error("Query:", input, " | Error:", error.message);
+  } else {
+    console.error("Query:", input, " | Error:", error);
+  }
+  Deno.exit();
 }
-
