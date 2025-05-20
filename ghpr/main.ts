853402f260b3bc8c60a22bd539678cbb4f58274a
Bevan Kay <email@bevankay.me>
2025-05-20 16:55:23 +1000

    ghpr/main.ts: actually fix range

--- ghpr/main.ts
+++ ghpr/main.ts
@@ -80,8 +80,7 @@ if (id.split("-").length > 1) {
     Deno.exit(1);
   }
 
-  // The `+ 1` adjustment ensures the range is inclusive of both `range[0]` and `range[1]`.
-  for (let x = range[0]; x <= range[1] + 1; x++) {
+  for (let x = range[0]; x <= range[1]; x++) {
     ids.push(x);
   }
 } else if (id.split("+").length > 1) {
@@ -103,7 +102,7 @@ if (id.split("-").length > 1) {
   ids = [];
   for (
     let x = start;
-    x < start + range;
+    x <= start + range;
     x++
   ) {
     ids.push(x);
